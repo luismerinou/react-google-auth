f0b3d03dd87b58bb2e6dfb33239f3bccaed83ca8
Luis Merino <luis.merino@autentia.com>
2023-01-27 13:58:04 +0100

    add google auth JWT Token

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import jwt_decode from "jwt-decode"
 import './App.css';
 
+
 function App() {
+
+  function handleCallbackResponse(response){
+      console.log("Encoded JWT ID TOKEN: " + response.credential)
+      var userObject = jwt_decode(response.credential)
+      console.log(userObject)
+  }
+
+  useEffect(() => {
+    /* global google from script in html*/
+    google.accounts.id.initialize({
+      client_id: "611628571049-e348cp3d7kgn9ndkdkj77paj7usj5pu4.apps.googleusercontent.com",
+      callback: handleCallbackResponse
+    });
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      {theme: "outline", size: "large"}
+    )
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="signInDiv"></div>
     </div>
   );
 }
